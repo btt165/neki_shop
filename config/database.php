44a475b4e1d64fb8c563ade26518d93deb76ab4c
btt165 <thanh.tam6999@gmail.com>
2025-08-19 13:59:33 +0700

    Update database.php for Heroku JawsDB

--- config/database.php
+++ config/database.php
@@ -2,36 +2,14 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url(env("DATABASE_URL", ""));
+
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
-    */
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
+
 
     'connections' => [
 
@@ -46,11 +24,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'] ?? env('DB_HOST', '127.0.0.1'),
+            'port' => $DATABASE_URL['port'] ?? env('DB_PORT', '3306'),
+            'database' =>isset($DATABASE_URL['path']) ? ltrim($DATABASE_URL['path'], "/") : env('DB_DATABASE', 'forge'),
+            'username' =>  $DATABASE_URL['user'] ?? env('DB_USERNAME', 'forge'),
+            'password' =>  $DATABASE_URL['pass'] ?? env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
